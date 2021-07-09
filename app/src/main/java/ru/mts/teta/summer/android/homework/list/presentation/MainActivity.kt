3b81a1063330b60725235a90676589d3b54bc5ed
SDudkin <sdudkin@stream.ru>
2021-07-09 17:33:52 +0300

    Added click listener and toasts

--- app/src/main/java/ru/mts/teta/summer/android/homework/list/presentation/MainActivity.kt
+++ app/src/main/java/ru/mts/teta/summer/android/homework/list/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package ru.mts.teta.summer.android.homework.list.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import coil.load
 import ru.mts.teta.summer.android.homework.list.R
 import ru.mts.teta.summer.android.homework.list.data.dto.MovieDto
@@ -18,12 +20,13 @@ class MainActivity : AppCompatActivity() {
 		
 		initDataSource()
 		setupViews()
+		setupListeners()
 	}
 	
 	private fun initDataSource() {
 		moviesModel = MoviesModel(MoviesDataSourceImpl())
 	}
-	
+
 	private fun setupViews() {
 		getMovieAt(MOVIES_INITIAL_POSITION)?.let {
 			// https://coil-kt.github.io/coil/
@@ -31,8 +34,16 @@ class MainActivity : AppCompatActivity() {
 			
 			findViewById<TextView>(R.id.tvTitle)?.text = it.title
 			findViewById<TextView>(R.id.tvDescription)?.text = it.description
-			findViewById<TextView>(R.id.tvRating)?.text = it.rateScore.toString()
-			findViewById<TextView>(R.id.tvAgeRestriction)?.text = "${it.ageRestriction}+"
+			findViewById<TextView>(R.id.tvRating)?.text =
+				getString(R.string.main_rating_text, it.rateScore)
+			findViewById<TextView>(R.id.tvAgeRestriction)?.text =
+				getString(R.string.main_age_restriction_text, it.ageRestriction)
+		}
+	}
+
+	private fun setupListeners() {
+		findViewById<View>(R.id.root)?.setOnClickListener {
+			showToast(getToastMessage())
 		}
 	}
 	
@@ -42,6 +53,17 @@ class MainActivity : AppCompatActivity() {
 			movies.isEmpty() -> null
 			position >= movies.size -> null
 			else -> movies[position]
+		}
+	}
+
+	private fun getToastMessage() = getMovieAt(MOVIES_INITIAL_POSITION)?.title?.let {
+		getString(R.string.main_click_message, it)
+	}
+
+	private fun showToast(message: String?) {
+		when {
+			message.isNullOrEmpty() -> { showToast(getString(R.string.main_empty_message)) }
+			else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 		}
 	}
 }
